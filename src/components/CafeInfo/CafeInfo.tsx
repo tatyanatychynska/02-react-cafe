c92a86e800d557b9c7d6456b5290ac65960e5656
Tanya Tychynska <tatyanazyeva@gmail.com>
2026-05-04 16:36:42 +0200

    HW02-react. Fixes for CafeInfo.tsx

--- src/components/CafeInfo/CafeInfo.tsx
+++ src/components/CafeInfo/CafeInfo.tsx
@@ -5,7 +5,7 @@ const CafeInfo = () => {
   return (<div className={css.container}>
     <h1 className={css.title}>Sip Happens Café</h1>
     <p className={css.description}>
-      Please rate our service by selecting one of the options below.
+      Please rate our service by selecting one of the options below
     </p>
   </div>);
 };
