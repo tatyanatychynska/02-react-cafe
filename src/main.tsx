f661f735f3a0d14d9372539688bdf71043335a1f
Tanya Tychynska <tatyanazyeva@gmail.com>
2026-05-04 16:26:24 +0200

    HW02-react. Fixes in main.ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import App from './components/App/App'
+import "modern-normalize";
 
 
 
 createRoot(document.getElementById('root') as HTMLDivElement).render(
-  <App app="app" />
+  <App />
 )
